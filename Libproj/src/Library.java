a1d30687be2965f0c5fd2f4ff241c4190cbda826
Vihanga <53087925+vihanga123@users.noreply.github.com>
2023-05-03 12:52:23 +0530

    simple login system

---
+++ Libproj/src/Library.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Library {
+    public static void main(String[] args) {
+
+        System.out.printf("Welcome to the library! Please Type '1' Login or '2' Register");
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        if (input.equals("1")) {
+            System.out.printf("Please enter your username");
+            String username = scanner.nextLine();
+            System.out.printf("Please enter your password");
+            String password = scanner.nextLine();
+            System.out.printf("Welcome %s", username);
+        } else if (input.equals("2")) {
+            System.out.printf("Please enter your name");
+            String name = scanner.nextLine();
+            System.out.printf("Please enter your password");
+            String password = scanner.nextLine();
+            System.out.printf("Welcome %s", name);
+        }
+
+
+    }
+}
---
+++ Libproj/src/books.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+public class books {
+        String[] books = {"The Lord of the Rings", "Harry Potter", "The Hobbit", "Jane Eyre"};
+        public void printBooks(){
+            System.out.println(Arrays.toString(books));
+        }
+}
+
+
+
