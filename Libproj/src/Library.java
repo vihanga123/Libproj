39b2a1f58ac07d5fc86cc782f679f9379ff01d93
Vihanga <53087925+vihanga123@users.noreply.github.com>
2023-05-07 20:14:47 +0530

    database connection made with Sqlite

--- Libproj/src/Library.java
+++ Libproj/src/Library.java
@@ -1,8 +1,35 @@
+import java.sql.DriverManager;
 import java.util.Scanner;
-import java.sql.Driver;
+import java.sql.*;
+
 public class Library {
 
+    //Creates the database using sqlite
+    public static void dbconnect(){
+        Connection conn = null;
+        try{
+            String url = "jdbc:sqlite:C:/sqlite/java/connect/library.db";
+            conn = DriverManager.getConnection(url);
+
+            System.out.println("Connection to SQLite has been established.");
+        } catch (SQLException e1) {
+            throw new RuntimeException(e1);
+        }
+        finally {
+            try {
+                if (conn != null) {
+                    conn.close();
+                }
+            } catch (SQLException e2) {
+                throw new RuntimeException(e2);
+            }
+        }
+    }
+
     public static void main(String[] args) {
+
+        //Calls the database connection code
+        dbconnect();
 
         System.out.printf("Welcome to the library! Please Type '1' Login or '2' Register");
         Scanner scanner = new Scanner(System.in);
