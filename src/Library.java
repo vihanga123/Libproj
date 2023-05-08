6c89c59b175c2431c50bd84cc1e9da8d0ade2455
Vihanga <53087925+vihanga123@users.noreply.github.com>
2023-05-08 21:29:48 +0530

    library database creation

--- src/Library.java
+++ src/Library.java
@@ -1,34 +1,46 @@
 import java.sql.DriverManager;
+import java.sql.Statement;
 import java.util.Scanner;
 import java.sql.*;
+
 
 public class Library {
 
     //Creates the database using sqlite
     public static void dbconnect(){
-        Connection conn = null;
-        try{
-            String url = "jdbc:sqlite:C:/sqlite/java/connect/library.db";
-            conn = DriverManager.getConnection(url);
 
+            String url = "jdbc:sqlite:C://sqlite/java/connect/library.db";
+
+            String sql = "CREATE TABLE IF NOT EXISTS books (\n"
+                    + " id integer PRIMARY KEY,\n"
+                    + " title text NOT NULL,\n"
+                    + " author text NOT NULL,\n"
+                    + " genre text NOT NULL,\n"
+                    + " pages integer NOT NULL\n"
+                    + ");";
+
+
+        try(Connection conn = DriverManager.getConnection(url);
+            Statement stmt = conn.createStatement())
+        {
+
+            //table creation
+            stmt.execute(sql);
             System.out.println("Connection to SQLite has been established.");
+
         } catch (SQLException e1) {
             throw new RuntimeException(e1);
         }
-        finally {
-            try {
-                if (conn != null) {
-                    conn.close();
-                }
-            } catch (SQLException e2) {
-                throw new RuntimeException(e2);
-            }
-        }
+
+
     }
 
     public static void main(String[] args) {
+        books book = new books();
+        book.printBooks();
 
-        //Calls the database connection code
+
+        //Calls the database connection method
         dbconnect();
 
         System.out.printf("Welcome to the library! Please Type '1' Login or '2' Register");
--- src/books.java
+++ src/books.java
@@ -1,6 +1,30 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.Arrays;
 public class books {
-        String[] books = {"The Lord of the Rings", "Harry Potter", "The Hobbit", "Jane Eyre"};
+    public static void bookdb(){
+        Connection conn = null;
+        try{
+            String url = "jdbc:sqlite:C:/sqlite/java/connect/books.db";
+            conn = DriverManager.getConnection(url);
+
+            System.out.println("Connection to SQLite has been established.");
+        } catch (SQLException e1) {
+            throw new RuntimeException(e1);
+        }
+        finally {
+            try {
+                if (conn != null) {
+                    conn.close();
+                }
+            } catch (SQLException e2) {
+                throw new RuntimeException(e2);
+            }
+        }
+    }
+
+    String[] books = {"The Lord of the Rings", "Harry Potter", "The Hobbit", "Jane Eyre"};
         public void printBooks(){
             System.out.println(Arrays.toString(books));
 
